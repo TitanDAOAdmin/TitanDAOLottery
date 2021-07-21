0989d6b9ff169847c58b82d6c5bc65288df67c0a
TitanDAOAdmin <86993382+TitanDAOAdmin@users.noreply.github.com>
2021-07-21 15:43:55 +1000

    Update TopBanner.tsx

--- src/views/DragonBall/components/TopBanner.tsx
+++ src/views/DragonBall/components/TopBanner.tsx
@@ -43,14 +43,14 @@ const TopBanner: React.FC<TopBannerProps> = ({
       <TopCountdownBanner small={notReady} className={notReady ? 'small' : ''}>
         <TopCountdownContent className="content">
           {notReady ? (
-            <h2>The next Competition to the Gods is not ready</h2>
+            <h2>The next Competition is not ready</h2>
           ) : notStarted ? (
             <h2>Round {lotteryId?.toString()} has not yet started</h2>
           ) : (
             <>
               <h4>Next Competition - Round {lotteryId?.toString()}</h4>
               {isPastClosing ? (
-                <DrawingContainer>Awaiting for results...</DrawingContainer>
+                <DrawingContainer>Awaiting results...</DrawingContainer>
               ) : (
                 <NextDrawCountdown to={closingTimestamp} />
               )}
