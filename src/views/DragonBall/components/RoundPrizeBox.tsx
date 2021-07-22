7dafbbf5263e7568498043fbf5f36c75a463c83d
TitanDAOAdmin <86993382+TitanDAOAdmin@users.noreply.github.com>
2021-07-22 18:25:10 +1000

    Update RoundPrizeBox.tsx

--- src/views/DragonBall/components/RoundPrizeBox.tsx
+++ src/views/DragonBall/components/RoundPrizeBox.tsx
@@ -16,7 +16,7 @@ const RoundPrizeBox: React.FC<RoundPrizeBoxProps> = ({ total, prizes, ticketsSol
   const jackpot = prizes ? prizes[0] : null;
   const matchFour = prizes ? prizes[1] : null;
   const matchThree = prizes ? prizes[2] : null;
-  const total70 = matchThree;
+  const total70 = total;
 
   return (
     <Box>
