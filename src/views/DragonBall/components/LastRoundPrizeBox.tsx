ea49d6b6d65c2db2e325f9838c60691d7f7a79cb
TitanDAOAdmin <86993382+TitanDAOAdmin@users.noreply.github.com>
2021-07-22 18:58:12 +1000

    Update LastRoundPrizeBox.tsx

--- src/views/DragonBall/components/LastRoundPrizeBox.tsx
+++ src/views/DragonBall/components/LastRoundPrizeBox.tsx
@@ -31,7 +31,7 @@ const LastRoundPrizeBox: React.FC<LastRoundPrizeBoxProps> = ({
 
   const matchThree7 = matchThree.mul(7);
   const matchThree70 = matchThree7.div(10);
-  
+
   return (
     <StyledWrapper>
       <StyledTotal>
@@ -41,7 +41,7 @@ const LastRoundPrizeBox: React.FC<LastRoundPrizeBoxProps> = ({
             <div className="prize-title">Total Rewards:</div>
           </div>
           <div className="prize-value">
-            <NumberDisplay value={total} decimals={18} precision={0} keepZeros={true} />
+            <NumberDisplay value={total70} decimals={18} precision={0} keepZeros={true} />
             <span className="prize-unit">TITAN</span>
           </div>
         </div>
@@ -58,7 +58,7 @@ const LastRoundPrizeBox: React.FC<LastRoundPrizeBoxProps> = ({
           <tr>
             <td className="column">Jackpot</td>
             <td className="column">
-              <NumberDisplay value={jackpot} decimals={18} precision={0} keepZeros={true} />
+              <NumberDisplay value={jackpot70} decimals={18} precision={0} keepZeros={true} />
               <span className="unit">TITAN</span>
             </td>
             <td className="column">{winners?.[0].toNumber()}</td>
@@ -66,7 +66,7 @@ const LastRoundPrizeBox: React.FC<LastRoundPrizeBoxProps> = ({
           <tr>
             <td className="column">Match 4</td>
             <td className="column">
-              <NumberDisplay value={matchFour} decimals={18} precision={0} keepZeros={true} />
+              <NumberDisplay value={matchFour70} decimals={18} precision={0} keepZeros={true} />
               <span className="unit">TITAN</span>
             </td>
             <td className="column">{winners?.[1].toNumber()}</td>
@@ -74,7 +74,7 @@ const LastRoundPrizeBox: React.FC<LastRoundPrizeBoxProps> = ({
           <tr>
             <td className="column">Match 3</td>
             <td className="column">
-              <NumberDisplay value={matchThree} decimals={18} precision={0} keepZeros={true} />
+              <NumberDisplay value={matchThree70} decimals={18} precision={0} keepZeros={true} />
               <span className="unit">TITAN</span>
             </td>
             <td className="column">{winners?.[2].toNumber()}</td>
