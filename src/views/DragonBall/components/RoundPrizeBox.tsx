565585fee85ef92d8e7ae65e73a0ee3ec7ee6f81
TitanDAOAdmin <86993382+TitanDAOAdmin@users.noreply.github.com>
2021-07-22 17:22:59 +1000

    Update RoundPrizeBox.tsx

--- src/views/DragonBall/components/RoundPrizeBox.tsx
+++ src/views/DragonBall/components/RoundPrizeBox.tsx
@@ -26,7 +26,7 @@ const RoundPrizeBox: React.FC<RoundPrizeBoxProps> = ({ total, prizes, ticketsSol
           <div className="content">
             <div>Current Total Rewards</div>
             <div className="prize-value">
-              <NumberDisplay value={total}*{2} decimals={18} precision={0} keepZeros={true} />
+              <NumberDisplay value={total*2} decimals={18} precision={0} keepZeros={true} />
               <span className="prize-unit">TITAN</span>
             </div>
           </div>
