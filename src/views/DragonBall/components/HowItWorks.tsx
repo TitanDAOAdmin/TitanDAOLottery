51940705d2fe2eaf510173d19a4478977de83e79
TitanDAOAdmin <86993382+TitanDAOAdmin@users.noreply.github.com>
2021-07-22 20:00:04 +1000

    Update HowItWorks.tsx

--- src/views/DragonBall/components/HowItWorks.tsx
+++ src/views/DragonBall/components/HowItWorks.tsx
@@ -26,34 +26,37 @@ const HowItWorks: React.FC<HowItWorksProps> = ({ costPerTicket }) => {
                 precision={0}
                 keepZeros={true}
               />{' '}
-              TITAN.{' '}
-              <a href={ExternalLinks.buyTitan} target="_blank">
-                Buy
-              </a>{' '}
+              TITAN.{' '}You can purchase up to 50 tickets per round.
+              {' '}
               TITAN first.
             </StyledContent>
           </StyledItem>
           <StyledItem>
-            <StyledOrder>3</StyledOrder>
+            <StyledOrder>2</StyledOrder>
             <StyledContent>
-              The competition will happen on Tuesday and Saturday at 2 PM UTC. You win if your Ticket number
-              matches at least 3 numbers.{' '}
+            If there are no winners per category, the prize accumulates to the next round. If there are multiple winners per category, the price is equally divided.{' '}
               <a href="https://titandao.gitbook.io/titandao/initiatives/initiatives" target="_blank">
-                See rules and example
+                See rules and examples
               </a>
               .
             </StyledContent>
           </StyledItem>
           <StyledItem>
+            <StyledOrder lastOrder>3</StyledOrder>
+            <StyledContent>
+              <div>15% of the prize pool is burned. Even when you do not win you are helping make TITAN great again!</div>
+            </StyledContent>
+          </StyledItem>
+          <StyledItem>
             <StyledOrder lastOrder>4</StyledOrder>
             <StyledContent>
-              <div>Reward allocation: </div>
-              <ul>
-                <li>Jackpot: 45% </li>
-                <li>Match 4: 35% </li>
-                <li>Match 3: 20% </li>
-              </ul>
-              In case of more winners in a category, the pot will be divided.
+              <div>The prizes are shown net of the 30% tax. The tax is used for token burns and to fund TitanDAO.</div>
+            </StyledContent>
+          </StyledItem>
+          <StyledItem>
+            <StyledOrder lastOrder>5</StyledOrder>
+            <StyledContent>
+              <div>There are 2 rounds per week, on Tuesdays and Saturdays at 2 pm UTC.</div>
             </StyledContent>
           </StyledItem>
         </StyledWrapper>
