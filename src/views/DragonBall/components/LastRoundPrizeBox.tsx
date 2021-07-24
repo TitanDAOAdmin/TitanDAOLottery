30fbacc7b8cff5772834dd4f6814609b3daa58e5
TitanDAOAdmin <86993382+TitanDAOAdmin@users.noreply.github.com>
2021-07-25 09:56:14 +1000

    Update LastRoundPrizeBox.tsx

--- src/views/DragonBall/components/LastRoundPrizeBox.tsx
+++ src/views/DragonBall/components/LastRoundPrizeBox.tsx
@@ -14,7 +14,7 @@ interface LastRoundPrizeBoxProps {
 
 const LastRoundPrizeBox: React.FC<LastRoundPrizeBoxProps> = ({ total, jackpot, matchFour, matchThree, winners }) => {
 
-    
+    const matchThree = pizza;
 
   return (
     <StyledWrapper>
